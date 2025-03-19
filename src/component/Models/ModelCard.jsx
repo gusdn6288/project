3af6889d2200422840819601658a94e26217edda
kim min seok <nas7062@naver.com>
2025-03-19 13:49:49 +0900

    feat(modelCard) : 페이지 이동 및 모델이름 넘겨주기

--- src/component/Models/ModelCard.jsx
+++ src/component/Models/ModelCard.jsx
@@ -7,17 +7,22 @@ const ModelCard = () => {
   const [models, setModels] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:8080/getAllModels") //  API 요청 유지
-      .then((response) => setModels(response.data))
-      .catch((error) => console.error("모델 정보를 불러오는 중 오류 발생:", error));
+    axios
+      .get("http://localhost:8080/getAllModels") //  API 요청 유지
+      .then((response) => {
+        setModels(response.data)
+      })
+      .catch((error) =>
+        console.error("모델 정보를 불러오는 중 오류 발생:", error)
+      );
   }, []);
 
   return (
-      <div className="Model-container">
-        {models.map((model) => (
-          <ModelCardItem key={model.id} model={model} />
-        ))}
-      </div>
+    <div className="Model-container">
+      {models.map((model) => (
+        <ModelCardItem key={model.id} model={model} />
+      ))}
+    </div>
   );
 };
 
